7126e2cec4db8f787a3a3f75d45d9ddd9ac2acaf
io-23SklyarAnton <80kap.i.toshka@gmail.com>
2023-09-10 15:40:05 +0300

    new_feature

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from web3 import Web3
 from collections import deque
+
 infura_url = 'https://mainnet.infura.io/v3/d6011d3466e84ae19605eb3af98af509'
 web3 = Web3(Web3.HTTPProvider(infura_url))
 
 gas_price = web3.eth.gas_price
 print(gas_price)
+
+
+def new_feature():
+    print("не вьебаться,какое важное обновление,прими пулл реквест пжпжпж")
